ef20b2de61cbdfd7b0b4dc46f4e87c92de7ca1b3
Wael Kdouh <waelkdouh@gmail.com>
2019-02-18 20:30:31 -0500

    Added the httpclient to the mvc application

--- MyApp.MVC/Controllers/HomeController.cs
+++ MyApp.MVC/Controllers/HomeController.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyApp.DAL.Models;
 using MyApp.MVC.Models;
+using Newtonsoft.Json;
 
 namespace MyApp.MVC.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+     
+        public ActionResult Index()
         {
-            return View();
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44334");
+                MediaTypeWithQualityHeaderValue contentType =new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                HttpResponseMessage response = client.GetAsync("/api/values").Result;
+                string stringData = response.Content.ReadAsStringAsync().Result;
+                List<Blog> data = JsonConvert.DeserializeObject<List<Blog>>(stringData);
+                return View(data);
+            }
         }
 
         public IActionResult Privacy()
